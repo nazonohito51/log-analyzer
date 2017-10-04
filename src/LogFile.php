073a12f3a8ef572c61d3b94b045e6aa94cac794c
kawashima <nazonohito51@gmail.com>
2017-10-04 18:19:38 +0900

    add LogFile class

---
+++ src/LogFile.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace LogAnalyzer;
+
+use Clover\Text\LTSV;
+use Kassner\LogParser\LogParser;
+
+/**
+ * ログファイル1個分を抽象化したクラス
+ * ログの保存形式はapache logでもltsvでもなんでも良い
+ * @package LogAnalyzer
+ */
+class LogFile
+{
+    /**
+     * @var LogParser|LTSV
+     */
+    private $parser;
+
+    private $file;
+    private $log_type;
+    private $options;
+
+    public function __construct($path, array $options = [])
+    {
+        $this->file = new \SplFileObject($path);
+        if (!$this->file->isFile()) {
+            throw new \InvalidArgumentException();
+        }
+
+        $this->log_type = $this->file->getExtension() == 'ltsv' ? 'ltsv' : 'apache';
+        if (isset($options['log_type'])) {
+            $this->log_type = $options['log_type'];
+        }
+        $this->options = [
+            'format' => isset($options['format']) ? $options['format'] : null
+        ];
+
+        if ($this->log_type == 'apache') {
+            $this->parser = new LogParser($this->options['format']);
+            // $parser->setFormat('%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"');
+        } elseif ($this->log_type == 'ltsv') {
+            $this->parser = new LTSV();
+        } else {
+            throw new \InvalidArgumentException('log_type is invalid.');
+        }
+    }
+
+    public function getEntry()
+    {
+        if ($this->file->eof()) {
+            return null;
+        }
+
+        $line = $this->file->fgets();
+        if ($this->log_type == 'apache') {
+            $iterable = $this->parser->parse($line);
+        } elseif ($this->log_type == 'ltsv') {
+            $iterable = $this->parser->parseLine($line);
+        } else {
+            throw new \LogicException('log_type is invalid.');
+        }
+
+        return new Entry($iterable);
+    }
+}
