754c92b32712fd23bbde59f592e35f57e5e7b706
kawashima <nazonohito51@gmail.com>
2017-10-05 19:25:24 +0900

    add display method to EntryAggregator

--- src/Aggregators/EntryAggregator.php
+++ src/Aggregators/EntryAggregator.php
@@ -2,6 +2,7 @@
 namespace LogAnalyzer\Aggregators;
 
 use LogAnalyzer\Entries\EntryInterface;
+use LucidFrame\Console\ConsoleTable;
 
 class EntryAggregator
 {
@@ -9,6 +10,8 @@ class EntryAggregator
      * @var EntryInterface[]
      */
     private $entries;
+
+    private $previousDimension = [];
 
     /**
      * @param EntryInterface[] $entries
@@ -21,6 +24,34 @@ class EntryAggregator
     public function count()
     {
         return count($this->entries);
+    }
+
+    public function display($dimension_property)
+    {
+        $result = [];
+        foreach ($this->entries as $entry) {
+            if ($entry->haveProperty($dimension_property)) {
+                $dimension_value = $entry->{$dimension_property};
+                $result[$dimension_value][] = $entry;
+            } else {
+                $result['null'][] = $entry;
+            }
+        }
+
+        $this->previousDimension = $result;
+        $this->displayTable($dimension_property, $result);
+    }
+
+    private function displayTable($dimension_property, array $dimension_result)
+    {
+        $table = new ConsoleTable();
+        $table->addHeader($dimension_property)
+            ->addHeader('Count');
+
+        foreach ($dimension_result as $property_value => $entries) {
+            $table->addRow()->addColumn($property_value)->addColumn(count($entries));
+        }
+        $table->display();
     }
 
     /**
--- tests/Aggregators/EntryAggregatorTest.php
+++ tests/Aggregators/EntryAggregatorTest.php
@@ -10,15 +10,14 @@ class EntryAggregatorTest extends TestCase
     public function testExtract()
     {
         $aggregator = new EntryAggregator([
-            new Entry(['key1' => 'value1', 'should_extract_key' => 1]),
-            new Entry(['key2' => 'value2']),
-            new Entry(['key3' => 'value3', 'should_extract_key' => 1]),
+            new Entry(['key' => 'value1', 'should_extract_key' => 1]),
+            new Entry(['key' => 'value2']),
+            new Entry(['key' => 'value3', 'should_extract_key' => 1]),
         ]);
 
         $new_aggregator = $aggregator->extract(function (EntryInterface $entry) {
             return $entry->haveProperty('should_extract_key');
         });
-
         $this->assertEquals(2, $new_aggregator->count());
     }
 }
