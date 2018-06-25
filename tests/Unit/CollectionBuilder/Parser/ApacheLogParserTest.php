824c3f04e7be96b43829ebb5dbd85f18e72a7837
kawashima <nazonohito51@gmail.com>
2018-06-25 17:27:46 +0900

    add Parser test

---
+++ tests/Unit/CollectionBuilder/Parser/ApacheLogParserTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace LogAnalyzer\CollectionBuilder\Parser;
+
+use LogAnalyzer\TestCase;
+
+class ApacheLogParserTest extends TestCase
+{
+    public function testParse()
+    {
+        $parser = new ApacheLogParser('%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"');
+
+        $ret = $parser->parse('133.130.35.34 - - [25/Jun/2017:04:02:05 +0900] "POST /users/1/articles HTTP/1.0" 200 346 "-" "Mozilla/5.0 (Windows CE) AppleWebKit/5350 (KHTML, like Gecko) Chrome/13.0.888.0 Safari/5350"');
+
+        $this->assertEquals('133.130.35.34', $ret['host']);
+        $this->assertEquals('-', $ret['user']);
+        $this->assertEquals('25/Jun/2017:04:02:05 +0900', $ret['time']);
+        $this->assertEquals('POST /users/1/articles HTTP/1.0', $ret['request']);
+        $this->assertEquals('200', $ret['status']);
+        $this->assertEquals('Mozilla/5.0 (Windows CE) AppleWebKit/5350 (KHTML, like Gecko) Chrome/13.0.888.0 Safari/5350', $ret['HeaderUserAgent']);
+    }
+}
---
+++ tests/Unit/CollectionBuilder/Parser/LtsvParser.php
@@ -0,0 +1,21 @@
+<?php
+namespace LogAnalyzer\CollectionBuilder\Parser;
+
+use LogAnalyzer\TestCase;
+
+class LtsvParserTest extends TestCase
+{
+    public function testParse()
+    {
+        $parser = new LtsvParser();
+
+        $ret = $parser->parse('date:2016-10-12 15:31:18	SCRIPT_NAME:/index.php	REQUEST_URI:/	REQUEST_METHOD:GET	HTTP_USER_AGENT:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36	included_files:bootstrap/autoload.php,public/index.php,app/Http/routes.php');
+
+        $this->assertEquals('2016-10-12 15:31:18', $ret['date']);
+        $this->assertEquals('/index.php', $ret['SCRIPT_NAME']);
+        $this->assertEquals('/', $ret['REQUEST_URI']);
+        $this->assertEquals('GET', $ret['REQUEST_METHOD']);
+        $this->assertEquals('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36', $ret['HTTP_USER_AGENT']);
+        $this->assertEquals('bootstrap/autoload.php,public/index.php,app/Http/routes.php', $ret['included_files']);
+    }
+}
