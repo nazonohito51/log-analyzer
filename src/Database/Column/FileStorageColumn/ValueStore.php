1454005c0d3c382a9f5b27742edc9f9cbe9b7310
kawashima <nazonohito51@gmail.com>
2018-11-08 10:16:17 +0900

    add ValueStore for simplify FileStorageColumn

---
+++ src/Database/Column/FileStorageColumn/ValueStore.php
@@ -0,0 +1,33 @@
+<?php
+namespace LogAnalyzer\Database\Column\FileStorageColumn;
+
+class ValueStore
+{
+    protected $values = [];
+
+    public function __construct(array $values)
+    {
+        foreach ($values as $value) {
+            $this->values[] = $value;
+        }
+    }
+
+    public function get($valueNo)
+    {
+        return isset($this->values[$valueNo]) ? $this->values[$valueNo] : null;
+    }
+
+    public function getValueNo($value)
+    {
+        $keys = array_keys($this->values, $value);
+
+        return count($keys) === 1 ? $keys[0] : $this->add($value);
+    }
+
+    protected function add($value)
+    {
+        $this->values[] = $value;
+
+        return count($this->values) - 1;
+    }
+}
---
+++ tests/Unit/Database/Column/FileStorageColumn/ValueStoreTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Tests\Unit\LogAnalyzer\Database\Column\FileStorageColumn;
+
+use LogAnalyzer\Database\Column\FileStorageColumn\ValueStore;
+use Tests\LogAnalyzer\TestCase;
+
+class ValueStoreTest extends TestCase
+{
+    public function testGet()
+    {
+        $store = new ValueStore(['value1', 'value2', 'value3']);
+
+        $this->assertEquals('value1', $store->get(0));
+        $this->assertEquals('value2', $store->get(1));
+        $this->assertEquals('value3', $store->get(2));
+        $this->assertNull($store->get(3));
+    }
+
+    public function testGetValueNo()
+    {
+        $store = new ValueStore(['value1', 'value2', 'value3']);
+
+        $this->assertEquals(0, $store->getValueNo('value1'));
+        $this->assertEquals(1, $store->getValueNo('value2'));
+        $this->assertEquals(2, $store->getValueNo('value3'));
+        $this->assertEquals(3, $store->getValueNo('value4'));
+    }
+}
