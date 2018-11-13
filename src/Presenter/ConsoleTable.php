6c3bcf152559112a2cddb1a606f45b973b039bf9
kawashima <nazonohito51@gmail.com>
2018-11-13 11:43:00 +0900

    enable limit row count of ConsoleTable

--- src/Presenter/ConsoleTable.php
+++ src/Presenter/ConsoleTable.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace LogAnalyzer\Presenter;
 
+use LogAnalyzer\Exception\InvalidArgumentException;
 use LogAnalyzer\View\ColumnStrategyInterface;
 use LucidFrame\Console\ConsoleTable as TableView;
 
@@ -10,6 +11,7 @@ class ConsoleTable
 {
     private $strategies;
     private $matrix;
+    private $rowLimit;
 
     /**
      * @param ColumnStrategyInterface[] $strategies
@@ -21,7 +23,7 @@ class ConsoleTable
         $this->matrix = $matrix;
     }
 
-    public function display($strLen = 60): void
+    public function display(int $strLen = 60): void
     {
         if (count($this->matrix) == 0) {
             echo "There is no data to show\n";
@@ -33,7 +35,11 @@ class ConsoleTable
         foreach ($this->strategies as $strategy) {
             $table->addHeader($strategy->name());
         }
-        foreach ($this->matrix as $row) {
+        foreach ($this->matrix as $cnt => $row) {
+            if (!is_null($this->rowLimit) && $this->rowLimit <= $cnt) {
+                break;
+            }
+
             $table->addRow();
             foreach ($this->strategies as $strategy) {
                 $table->addColumn($this->formatValue($row[$strategy->name()], $strLen));
@@ -64,11 +70,20 @@ class ConsoleTable
         return count($arr) > 1 ? "[{$str}]" : $str;
     }
 
-    public function sort($columnName, $orderByDesc = false): self
+    public function sort(string $columnName, $orderByDesc = false): self
     {
         $sortColumn = array_column($this->matrix, $columnName);
         array_multisort($sortColumn, $orderByDesc ? SORT_DESC : SORT_ASC, $this->matrix);
 
         return $this;
+    }
+
+    public function limit(int $limit)
+    {
+        if ($limit <= 0) {
+            throw new InvalidArgumentException('limit must be greater than 0.');
+        }
+
+        $this->rowLimit = $limit;
     }
 }
